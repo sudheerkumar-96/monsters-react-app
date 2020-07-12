61cfd2dce7247eb69c26302a4b4118c6671ea1e2
sudheer kumar <sudheerkvy1996@gmail.com>
2020-07-12 03:29:47 -1200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters:[],
+      searchField: ''
+    };
+    //this.handlechange=this.handlechange.bind(this);
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(Response=>Response.json())
+    .then(users=>this.setState({monsters:users}));
+  }
+  handlechange = (e)=>
+  //here e is a synthetic event,React defines these synthetic events 
+  //according to the W3C spec, 
+  //so you don’t need to worry about cross-browser compatibility.
+  {
+    this.setState({searchField: e.target.value})
+  }
+  render(){
+    const {monsters,searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+       monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+         <SearchBox
+           placeholder='search monsters'
+           handlechange={this.handlechange}
+         />
+        <CardList monsters={filteredMonsters}>
+        </CardList>
+        
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/card/card.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Card = (props) =>   (
+<h1>{props.monsters.name}</h1>
+)
