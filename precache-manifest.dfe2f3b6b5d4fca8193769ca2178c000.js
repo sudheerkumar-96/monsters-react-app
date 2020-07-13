88a52e3b87ca2fdadfdb2536af3f4b0f01f6c994
sudheerkumar1996 <sudheerkvy1996@gmail.com>
2020-07-13 06:09:57 -1200

    Updates

---
+++ precache-manifest.dfe2f3b6b5d4fca8193769ca2178c000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b6b44805098f83ee699325eb8f5ac9",
+    "url": "/monsters-react-app/index.html"
+  },
+  {
+    "revision": "5450bc8e2fc154c86972",
+    "url": "/monsters-react-app/static/css/main.412f2258.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-react-app/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react-app/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5450bc8e2fc154c86972",
+    "url": "/monsters-react-app/static/js/main.6e059388.chunk.js"
+  },
+  {
+    "revision": "44065339f7f8830ecd3b",
+    "url": "/monsters-react-app/static/js/runtime-main.28a821a8.js"
+  }
+]);
